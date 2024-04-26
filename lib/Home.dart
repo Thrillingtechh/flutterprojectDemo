b1f8d79ce6944f19fb7d91c94ecb81f49b1fe82a
thrilling techh <thrillingtechofficial@gmail.com>
2024-04-26 10:26:29 -0700

    chnage

--- lib/Home.dart
+++ lib/Home.dart
@@ -6,7 +6,7 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return  Scaffold(
-      appBar: AppBar(title: Text('HomeScreen',)),
+      appBar: AppBar(title: Text('MasterScreen',)),
     );
   }
 }
